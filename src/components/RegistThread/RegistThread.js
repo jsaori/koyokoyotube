2ae412d2d2932fd8844dab46f9b30725b8d7cfd1
jsaori <saori.koubou@gmail.com>
2023-01-08 14:23:57 +0900

    shitaraba

--- src/components/RegistThread/RegistThread.js
+++ src/components/RegistThread/RegistThread.js
@@ -83,15 +83,15 @@ export const RegistThread = memo(({ sx, defaultYoutubeURL="" }) => {
         yup.object().shape({
           url: yup
             .string()
-            .required("5ch実況スレ URL は必須です")
+            .required("実況スレ URL は必須です")
             .url("URLが不正です")
             .max(STRING_MAX, "URLが不正です")
-            .matches(/(5ch.net).+\/([0-9]{10})($|\/)/, { message: "URLが不正です"})
+            .matches(/((5ch.net).+\/([0-9]{10})|((shitaraba.net).+\/25835\/([0-9]{10})))($|\/)/, { message: "URLが不正です"})
             .test("duplicated-threads", "実況スレURLが重複しています", validateDuplicatedThreads)
         }),
       )
-      .min(THREAD_MIN, `5ch実況スレ URL: ${THREAD_MIN}以上の登録が必要です`)
-      .max(THREAD_MAX, `5ch実況スレ URL: ${THREAD_MAX}以下の登録が必要です`)
+      .min(THREAD_MIN, `実況スレ URL: ${THREAD_MIN}以上の登録が必要です`)
+      .max(THREAD_MAX, `実況スレ URL: ${THREAD_MAX}以下の登録が必要です`)
   });
 
   const {
@@ -279,7 +279,7 @@ export const RegistThread = memo(({ sx, defaultYoutubeURL="" }) => {
               <LockIcon />
             </IconButton>
             <FlexTextForm
-              label={`[登録済み]5ch実況スレ URL${index + 1}`}
+              label={`[登録済み]実況スレ URL${index + 1}`}
               variant="standard"
               value={thread.url}
               type="url"
@@ -302,7 +302,7 @@ export const RegistThread = memo(({ sx, defaultYoutubeURL="" }) => {
                   <ClearIcon />
                 </IconButton>
                 <FlexTextForm
-                  label={`5ch実況スレ URL${index + 1}`}
+                  label={`実況スレ URL${index + 1}`}
                   variant="standard"
                   type="url"
                   // threadurlは必須
