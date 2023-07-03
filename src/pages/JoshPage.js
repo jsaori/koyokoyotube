a021c4d7c9a6b4a9fa9163259824dae0343ef7d1
jsaori <saori.koubou@gmail.com>
2023-07-03 10:36:14 +0900

    Skip password entry if already authenticated

--- src/pages/JoshPage.js
+++ src/pages/JoshPage.js
@@ -8,15 +8,19 @@ import { useLocalStorage } from "../hooks/useLocalStrage";
  */
  export default function JoshPage() {
   // Josh判定フラグ
-  const [, setJosh] = useLocalStorage('josh', 'false');
-
-  // パスワード確認
-  const pass = window.prompt("パスワード", "");
+  // 既に認証済みであればパスワード入力をスキップ
+  const [isJosh, setJosh] = useLocalStorage('josh', 'false');
 
   const navigate = useNavigate();
   useEffect(() => {
-    // パスワードでJoshフラグTrue
-    if (pass === process.env.REACT_APP_JOSH_KEY) setJosh('true');
+    if (isJosh === 'false') {
+      // パスワード確認
+      const pass = window.prompt("パスワード", "");
+
+      // パスワードでJoshフラグTrue
+      if (pass === process.env.REACT_APP_JOSH_KEY) setJosh('true');
+    }
+
     // 初期ページに遷移
     navigate("/channel/koyori")
   });
